fc2ccd62bef9ed4f8837f119283871c19d0b3fde
Rustcrim <anshikasiwanch@gmail.com>
2024-04-14 17:06:44 +1000

    login changes

--- src/main/java/com/example/login/HelloApplication.java
+++ src/main/java/com/example/login/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("login-view.fxml")); // /com/example/login/login-view.fxml
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Login User");
+        stage.setTitle("Login user my name is anshika");
         stage.setScene(scene);
         stage.show();
     }
